f41a6c08051408d170c661b1d11ff25484b4b901
miniecloud <y.minihee@gmail.com>
2024-04-25 22:18:00 +0900

    feat : 댓글 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def post_list(request):
@@ -23,5 +25,20 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day) 
+    # active 댓글 목록
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html',
+                   {'post':post, 'comments':comments, 'form':form})
 
-    return render(request, 'blog/post/detail.html', {'post':post})
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', 
+                  {'post':post, 'form':form, 'comment':comment})
